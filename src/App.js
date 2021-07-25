8f54fc2e3691fbb7adfdfae337f6d35d68f884da
rahmamansouri <rahma.mansouri@etudiant-fst.utm.tn>
2021-07-25 04:36:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from "./Header.js"
+import Item from "./Item.js"
 import './App.css';
 
-function App() {
+
+class App extends React.Component {
+
+constructor(props) {
+  super(props)
+  this.state = {
+
+    newItemValue: "",
+
+
+items: [
+  { text: "First Todo", done: false },
+  { text: "Second Todo", done: true },
+
+
+]
+
+  }
+
+
+this.handleChange = this.handleChange.bind(this)
+this.handleSubmit = this.handleSubmit.bind(this)
+this.toggleTodo = this.toggleTodo.bind(this)
+
+}
+
+handleChange(event){
+this.setState({ newItemValue: event.target.value })
+}
+
+
+
+handleSubmit(event)
+{
+
+event.preventDefault();
+let items = this.state.items.slice()
+
+
+items.push({
+
+text: this.state.newItemValue,
+done: false,
+
+})
+
+this.setState({
+newItemValue: '',
+items: items
+
+
+})
+
+
+}
+
+
+toggleTodo(index) {
+
+  let items = this.state.items.slice()
+  let item = items[index]
+  item.done = !item.done 
+  this.setState ({
+
+items: items
+
+})
+
+}
+
+
+
+
+
+
+render (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header/>
+
+<form onSubmit={this.handleSubmit}>
+
+<input type="text" value={this.state.newItemValue} placeholder="New Todo" onChange={this.handleChange}/>
+<input type="submit" value="Add Item"/>
+
+</form>
+
+
+
+
+
+
+<ol>
+  {this.state.items.map((item, index) => (
+
+
+<Item key={index} clickHandler={()  => this.toggleTodo(index) } done={item.done} text={item.text} />
+
+  ))}
+
+</ol>
+</div>
   );
 }
+}
+
+
+
 
 export default App;
---
+++ src/Item.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Item = (props) => {
+
+
+    return (
+
+        <li>  
+        <label>
+        <input onClick={props.clickHandler} type="checkbox" checked={props.done || false} />
+        <span>{props.text}</span>
+        
+        </label>
+        
+        
+        </li>
+        
+        
+            )
+        
+
+
+
+
+
+
+}
+
+
+
+
+
+
+export default Item  ;
